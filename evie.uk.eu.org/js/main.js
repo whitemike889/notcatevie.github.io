9c33edfd4d6a614c038fb01005a30d16afee13dd
Evie Burrows <44758355+NotCatEvie@users.noreply.github.com>
2018-11-05 19:52:53 +0100

    Add static for evie.uk.eu.org

---
+++ evie.uk.eu.org/js/main.js
@@ -0,0 +1,7 @@
+// Include JS by Ayako
+
+function include(fileName){
+  document.write("<script src='https://static.evie.uk.eu.org/evie.uk.eu.org/js/"+fileName+".js'></script>" ); // your link ex: static.ayako.dedigo.fr/js/filename
+}
+
+include('bootstrap');
