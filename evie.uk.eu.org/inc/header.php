ba4bae759ea5793254472f93772ade4693176cab
Evie Burrows <44758355+NotCatEvie@users.noreply.github.com>
2018-11-13 22:30:14 +0100

    Add files via upload

---
+++ evie.uk.eu.org/inc/header.php
@@ -0,0 +1,9 @@
+	<div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+      <h5 class="my-0 mr-md-auto font-weight-normal">NotCatEvie</h5>
+      <nav class="my-2 my-md-0 mr-md-3">
+        <a class="p-2 text-dark" href="<?php $_SEREVR['PHP_SELF']; ?>home.phtml">Home</a>
+        <a class="p-2 text-dark" href="<?php $_SEREVR['PHP_SELF']; ?>tools.phtml">Tools</a>
+        <a class="p-2 text-dark" href="<?php $_SEREVR['PHP_SELF']; ?>partners.phtml">Partners</a>
+      </nav>
+      <a class="btn btn-outline-primary" href="<?php $_SEREVR['PHP_SELF']; ?>social.phtml">Social Network</a>
+  </div>
