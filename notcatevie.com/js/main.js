fe40f3552191ee8cf0655b621201a07324849ecc
Evie Burrows <44758355+NotCatEvie@users.noreply.github.com>
2018-11-14 20:40:01 +0100

    change s cdn

--- notcatevie.com/js/main.js
+++ notcatevie.com/js/main.js
@@ -1,7 +1,7 @@
 // Include JS by Ayako
 
 function include(fileName){
-  document.write("<script src='https://static.evie.uk.eu.org/evie.uk.eu.org/js/"+fileName+".js'></script>" ); // your link ex: static.ayako.dedigo.fr/js/filename
+  document.write("<script src='https://s.notcatevie.com/notcatevie.com/js/"+fileName+".js'></script>" ); // your link ex: static.ayako.dedigo.fr/js/filename
 }
 
 include('bootstrap');
